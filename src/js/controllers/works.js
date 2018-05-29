c0ff993a4c9eabb15aaae86d9732d2e16d6f3774
peter <peter@soez.io>
2018-05-29 16:42:17 +0800

    修改待回执控制器名称

--- src/js/controllers/works.js
+++ src/js/controllers/works.js
@@ -1,8 +1,11 @@
 angular.module('app')
-    .controller('ReceiptsTodoController', ['$scope', '$state', '$stateParams', 'commonService', function($scope, $state, $stateParams, commonService) {
+    .controller('ReceiptsTodoListController', ['$scope', '$state', '$stateParams', 'commonService', function($scope, $state, $stateParams, commonService) {
         $scope.receipts = commonService.getList();
     }])
     .controller('ReceiptSolveController', ['$scope', '$state', '$stateParams', 'commonService', function($scope, $state, $stateParams, commonService) {
         $scope.item = commonService.getItem(parseInt($stateParams.id));
         console.log($scope.item);
+    }])
+    .controller('ReceiptsDoneListController', ['$scope', '$state', '$stateParams', 'commonService', function($scope, $state, $stateParams, commonService) {
+        $scope.receipts = commonService.getList();
     }]);
